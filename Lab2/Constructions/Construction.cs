022ab0f5fe846c02feefcc78ae9eb01e6357a767
Anastasiya Bialkevich <w68368@student.wsiz.edu.pl>
2023-10-19 15:49:48 +0200

    Lab2

---
+++ Lab2/Constructions/Construction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.Constructions
+{
+    class Construction
+    {
+        public Construction(double height, double width, int entrances, int humanCapacity, string buildMaterial)  // ctor tab*2
+        {
+            Height = height;
+            Width = width;
+            Entrances = entrances;
+            HumanCapacity = humanCapacity;
+            BuildMaterial = buildMaterial;
+        }
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public int Entrances { get; set; }
+        public int HumanCapacity { get; set; }
+        public string BuildMaterial { get; set; }
+
+    }
+}
+
---
+++ Lab2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Lab2.Constructions;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var construction = new Construction(23, 45, 4, 6, "Brik");
+            Console.WriteLine(construction.BuildMaterial);
+        }
+    }
+}
